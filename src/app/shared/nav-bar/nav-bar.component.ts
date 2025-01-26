fa6ce072c1567d33b4a5cd296d95248bc90ad496
Felipe Costa <felipe95176@gmail.com>
2025-01-26 13:45:45 -0300

    Add reposivite.

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent {}
