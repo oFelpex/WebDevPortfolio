9f549031ce62ad2def9c0f30d329c09700fcae7a
Felipe Costa <felipe95176@gmail.com>
2025-03-09 16:35:36 -0300

    ✨ Add the icons to angular material.

--- src/app/shared/social-links/social-links.component.ts
+++ src/app/shared/social-links/social-links.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { MatIconRegistry } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-social-links',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './social-links.component.html',
-  styleUrl: './social-links.component.scss'
+  styleUrl: './social-links.component.scss',
 })
 export class SocialLinksComponent {
+  constructor() {
+    const matIconRegistry = inject(MatIconRegistry);
+    const domSanitizer = inject(DomSanitizer);
 
+    const icons = [
+      'icon-linkedin',
+      'icon-whatsapp',
+      'icon-github',
+      'icon-twitter',
+    ];
+
+    icons.forEach((icon) => {
+      matIconRegistry.addSvgIcon(
+        icon,
+        domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/${icon}.svg`)
+      );
+    });
+  }
 }
