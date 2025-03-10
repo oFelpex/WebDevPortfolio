a9159b7b0716283c78d2e0a0e857b14a945b208e
Felipe Costa <felipe95176@gmail.com>
2025-03-10 11:23:42 -0300

    ✨ Add a footer that show the icons of social links

--- src/app/shared/mobile-menu/mobile-menu.component.ts
+++ src/app/shared/mobile-menu/mobile-menu.component.ts
@@ -7,15 +7,22 @@ import { MobileThemeMenuSheetComponent } from './mobile-theme-menu/mobile-theme-
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { SocialLinksComponent } from '../social-links/social-links.component';
 
 @Component({
   selector: 'app-mobile-menu',
-  imports: [MatSidenavModule, MatListModule, RouterModule],
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    SocialLinksComponent,
+  ],
   templateUrl: './mobile-menu.component.html',
   styleUrl: './mobile-menu.component.scss',
 })
 export class MobileMenuComponent {
   @ViewChild('mobileMenu') mobileMenu!: MatDrawer;
+  public mobileSvgIconsColor: string = '#FFF';
 
   constructor(private mobileMenuService: MobileMenuService) {}
 
