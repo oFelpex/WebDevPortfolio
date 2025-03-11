57d174eb6205cb8d74fbb3283236bc70e85562a3
Felipe Costa <felipe95176@gmail.com>
2025-03-11 16:07:22 -0300

    ✨ Create the template for the card projects

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from '../../shared/header/header.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-projects',
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
