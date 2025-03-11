103d4a4ad22aa9e35acf111eb112462ccfeca047
Felipe Costa <felipe95176@gmail.com>
2025-03-11 11:15:19 -0300

    ✨ Add a header to projects page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {
-
-}
+export class ProjectsComponent {}
