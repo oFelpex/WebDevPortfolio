bef0e8b622079428d7a6000e8dc4f7e670f57b7a
Felipe Costa <felipe95176@gmail.com>
2025-03-13 14:07:00 -0300

    🎨 Add a @for, so can use the array of projects. Add chips from angular material for styling and show the hardskills used on each project.

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 import { HeaderComponent } from '../../shared/header/header.component';
+import { allProjects, Projects } from '../../models/projects';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-projects',
@@ -15,8 +17,18 @@ import { MatDividerModule } from '@angular/material/divider';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
+    MatChipsModule,
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  public projects: Projects[] = allProjects;
+
+  mouseIsOver = false;
+
+  @HostListener('mouseenter')
+  isMouseOverImg() {
+    this.mouseIsOver = true;
+  }
+}
