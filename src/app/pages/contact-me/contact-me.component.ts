9c8398756d37581b28312f92882f9556c6d1e0e9
Felipe Costa <felipe95176@gmail.com>
2025-03-14 09:14:54 -0300

    ✨ Create the skeleton of the page, adding header and a new component.

--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { ContactMeFormComponent } from './contact-me-form/contact-me-form.component';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [HeaderComponent, ContactMeFormComponent],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
-export class ContactMeComponent {
-
-}
+export class ContactMeComponent {}
