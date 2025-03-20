6c0042e999d9c1a0073a4dfd7faa677d3ae68c0c
Felipe Costa <felipe95176@gmail.com>
2025-03-20 16:19:37 -0300

    🏷️ Create the interface Photos and a array of it, allPhotos.

---
+++ src/app/models/photos.ts
@@ -0,0 +1,27 @@
+export interface Photos {
+  imgURL: string;
+  imgAlt: string;
+}
+export const allPhotos: Photos[] = [
+  {
+    imgURL: '../../assets/images/about-me/cards/mia-0.webp',
+    imgAlt: 'My cat showing her teeth, scary.',
+  },
+  {
+    imgURL: '../../assets/images/about-me/cards/amora.webp',
+    imgAlt:
+      'My mixed-breed dog, her name is Amora, in Brazil we call cachorro caramelo (caramel dog in english).',
+  },
+  {
+    imgURL: '../../assets/images/about-me/cards/mia-1.webp',
+    imgAlt: `It's Mia again, now it's a full photo, she's a gray cat.`,
+  },
+  {
+    imgURL: '../../assets/images/about-me/cards/myself-again.webp',
+    imgAlt: `It's me again, now I'm using a bluer t-shirt, smiling to camera.`,
+  },
+  {
+    imgURL: '../../assets/images/about-me/cards/mia-2.webp',
+    imgAlt: `Oh, It's Mia... Again, yeah, I realy love Mia, in this photo she's sleeping hold her teddy bear.`,
+  },
+];
