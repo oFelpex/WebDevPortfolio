7fefa92e4c8743c2f9d0025d6d540322ba76c06c
Felipe Costa <felipe95176@gmail.com>
2025-03-20 16:20:07 -0300

    💄 Create and generate all photos.

--- src/app/pages/about-me/photos-footer/photos-footer.component.ts
+++ src/app/pages/about-me/photos-footer/photos-footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { allPhotos, Photos } from '../../../models/photos';
+
 @Component({
   selector: 'app-photos-footer',
   imports: [],
   templateUrl: './photos-footer.component.html',
-  styleUrl: './photos-footer.component.scss'
+  styleUrl: './photos-footer.component.scss',
 })
 export class PhotosFooterComponent {
-
+  allPhotos: Photos[] = allPhotos;
 }
