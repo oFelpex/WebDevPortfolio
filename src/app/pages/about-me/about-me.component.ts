aea0c0973ae8ad97853cffdb255679c7e6320756
Felipe Costa <felipe95176@gmail.com>
2025-03-20 15:18:00 -0300

    💄 Completly redesign the about-me page

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { SocialLinksComponent } from '../../shared/social-links/social-links.component';
 
+import { MatDividerModule } from '@angular/material/divider';
+
 @Component({
   selector: 'app-about-me',
-  imports: [HeaderComponent, SocialLinksComponent],
+  imports: [HeaderComponent, SocialLinksComponent, MatDividerModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
