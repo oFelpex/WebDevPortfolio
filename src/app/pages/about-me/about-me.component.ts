d1c015e17187928ce5ba6beb4f638af5f2923f7f
Felipe Costa <felipe95176@gmail.com>
2025-03-21 20:57:55 -0300

    💫 Add a animation for the first and second part of about-me

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FirstHalfComponent } from './first-half/first-half.component';
@@ -6,6 +6,14 @@ import { SecondHalfComponent } from './second-half/second-half.component';
 import { PhotosFooterComponent } from './photos-footer/photos-footer.component';
 
 import { MatDividerModule } from '@angular/material/divider';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { LoadingService } from '../../services/loading-service/loading.service';
 
 @Component({
   selector: 'app-about-me',
@@ -18,5 +26,31 @@ import { MatDividerModule } from '@angular/material/divider';
   ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
+  animations: [
+    trigger('fadeInUpToDown', [
+      state('loading', style({ opacity: 0, transform: 'translateY(-100px)' })),
+      state(
+        'loaded',
+        style({
+          opacity: 1,
+          transform: 'translateY(0px)',
+        })
+      ),
+      transition('loading => loaded', animate('300ms ease-in')),
+    ]),
+  ],
 })
-export class AboutMeComponent {}
+export class AboutMeComponent implements OnInit {
+  private loadingService: LoadingService;
+
+  constructor() {
+    this.loadingService = inject(LoadingService);
+  }
+
+  isLoading = false;
+  ngOnInit() {
+    this.loadingService.loading$.subscribe((loading) => {
+      this.isLoading = loading;
+    });
+  }
+}
