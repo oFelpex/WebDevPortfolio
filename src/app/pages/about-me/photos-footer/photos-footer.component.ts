fd7393ca514c3ee9e603d49abffaf6922503bd6f
Felipe Costa <felipe95176@gmail.com>
2025-03-21 20:58:15 -0300

    💫 Add a animation for photos-footer

--- src/app/pages/about-me/photos-footer/photos-footer.component.ts
+++ src/app/pages/about-me/photos-footer/photos-footer.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { allPhotos, Photos } from '../../../models/photos';
+import {
+  animate,
+  query,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-photos-footer',
   imports: [],
   templateUrl: './photos-footer.component.html',
   styleUrl: './photos-footer.component.scss',
+  animations: [
+    trigger('fadeInDownToUp', [
+      transition(':enter', [
+        query(
+          '.photo-container',
+          [
+            style({ opacity: 0, transform: 'translateY(100px)' }),
+            stagger(100, [
+              animate(
+                '200ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class PhotosFooterComponent {
   allPhotos: Photos[] = allPhotos;
