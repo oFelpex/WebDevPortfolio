7bcc495407c6ed9856c634e87ce5cf873160d59a
Felipe Costa <felipe95176@gmail.com>
2025-03-25 15:10:34 -0300

    🌐 Add translate for intro-section (not the cards)

--- src/app/pages/home/intro-section/intro-section.component.ts
+++ src/app/pages/home/intro-section/intro-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
 import { NavigationCardsComponent } from './navigation-cards/navigation-cards.component';
 import { getDownToUp_getUptoDown } from '../../../shared/animations/translate-animations';
@@ -7,26 +7,73 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 import Typed from 'typed.js';
+import {
+  LangChangeEvent,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-intro-section',
-  imports: [MatButtonModule, MatIconModule, NavigationCardsComponent],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    NavigationCardsComponent,
+    TranslateModule,
+  ],
   templateUrl: './intro-section.component.html',
   styleUrl: './intro-section.component.scss',
   animations: [getDownToUp_getUptoDown],
 })
-export class IntroSectionComponent implements OnInit {
+export class IntroSectionComponent implements OnInit, OnDestroy {
+  translate: TranslateService;
+  private langSubscription!: Subscription;
+
+  constructor() {
+    this.translate = inject(TranslateService);
+  }
+
   ngOnInit(): void {
-    new Typed('.auto-type', {
-      strings: [
-        'Front-end Developer^2500',
-        'Web Developer^2500',
-        'student^4000',
-      ],
+    let typedInstance: Typed;
+    let EN_autoTypeArr: string[] = [
+      'Front end developer^2500',
+      'Web developer^2500',
+      'student^4000',
+    ];
+    let ptBR_autoTypeArr: string[] = [
+      'Desenvolvedor front end^2500',
+      'Desenvolvedor web^2500',
+      'Estudante^4000',
+    ];
+    let autoTypeArr: string[] = [];
+
+    this.translate.currentLang === 'en'
+      ? (autoTypeArr = EN_autoTypeArr)
+      : (autoTypeArr = ptBR_autoTypeArr);
+
+    typedInstance = new Typed('.auto-type', {
+      strings: [...autoTypeArr],
       typeSpeed: 100,
       backSpeed: 30,
       loop: true,
     });
+
+    this.langSubscription = this.translate.onLangChange.subscribe(
+      (event: LangChangeEvent) => {
+        event.lang === 'en'
+          ? (autoTypeArr = EN_autoTypeArr)
+          : (autoTypeArr = ptBR_autoTypeArr);
+        typedInstance.destroy();
+
+        typedInstance = new Typed('.auto-type', {
+          strings: [...autoTypeArr],
+          typeSpeed: 100,
+          backSpeed: 30,
+          loop: true,
+        });
+      }
+    );
   }
 
   showNavigationCards: boolean = false;
@@ -43,5 +90,9 @@ export class IntroSectionComponent implements OnInit {
     this.showNavigationCards
       ? (introSectionButtonIcon.style.transform = 'rotate(180deg)')
       : (introSectionButtonIcon.style.transform = 'rotate(0deg)');
+  }
+
+  ngOnDestroy(): void {
+    this.langSubscription.unsubscribe();
   }
 }
