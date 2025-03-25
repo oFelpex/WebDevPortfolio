20013a08aef010d06c8019624453a41de7742790
Felipe Costa <felipe95176@gmail.com>
2025-03-25 15:21:40 -0300

    🌐 Add translate for navigation-cards

--- src/app/pages/home/intro-section/navigation-cards/navigation-cards.component.ts
+++ src/app/pages/home/intro-section/navigation-cards/navigation-cards.component.ts
@@ -5,9 +5,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 import { fadeInDownToUp_fadeOutUpToDown } from '../../../../shared/animations/fade-animations';
+
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-navigation-cards',
-  imports: [RouterModule, MatCardModule, MatButtonModule],
+  imports: [RouterModule, MatCardModule, MatButtonModule, TranslateModule],
   templateUrl: './navigation-cards.component.html',
   styleUrl: './navigation-cards.component.scss',
   animations: [fadeInDownToUp_fadeOutUpToDown],
