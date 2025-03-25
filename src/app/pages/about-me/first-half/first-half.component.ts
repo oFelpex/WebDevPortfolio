b7f47883bb149f821ef005ec558c0cf722479ec8
Felipe Costa <felipe95176@gmail.com>
2025-03-25 15:34:05 -0300

    🌐 Add translate for first-half of about-me page

--- src/app/pages/about-me/first-half/first-half.component.ts
+++ src/app/pages/about-me/first-half/first-half.component.ts
@@ -4,9 +4,10 @@ import { SocialLinksComponent } from '../../../shared/social-links/social-links.
 
 import { MatDividerModule } from '@angular/material/divider';
 
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-first-half',
-  imports: [SocialLinksComponent, MatDividerModule],
+  imports: [SocialLinksComponent, MatDividerModule, TranslateModule],
   templateUrl: './first-half.component.html',
   styleUrl: './first-half.component.scss',
 })
