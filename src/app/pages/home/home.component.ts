61be64f6ca47c2dfa87e601496e3a323fb8da893
Felipe Costa <felipe95176@gmail.com>
2025-03-31 19:27:42 -0300

    💫 Add the new animation fadeIn_opacity_loading to intro section app

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
 import { HeaderComponent } from '../../shared/header/header.component';
 import { IntroSectionComponent } from './intro-section/intro-section.component';
+import { fadeIn_opacity_loading } from '../../shared/animations/fade-animations';
+import { LoadingService } from '../../services/loading-service/loading.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   imports: [HeaderComponent, IntroSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  animations: [fadeIn_opacity_loading],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  private loadingService: LoadingService;
+  public isLoading!: Boolean;
+  private loadingSubscription!: Subscription;
+
+  constructor() {
+    this.loadingService = inject(LoadingService);
+  }
+
+  ngOnInit() {
+    this.loadingSubscription = this.loadingService.loading$.subscribe(
+      (loading) => {
+        this.isLoading = loading;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSubscription.unsubscribe();
+  }
+}
