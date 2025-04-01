f24c7ca02cea06338932f50feec504a4c74b5b2f
Felipe Costa <felipe95176@gmail.com>
2025-04-01 14:48:10 -0300

    ✨ It's added the page: Not Found, including the translate of it

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  imports: [HeaderComponent, RouterModule, TranslateModule, MatButtonModule],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  styleUrl: './page-not-found.component.scss',
 })
-export class PageNotFoundComponent {
-
-}
+export class PageNotFoundComponent {}
