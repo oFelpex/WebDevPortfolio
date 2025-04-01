dea5d2ed431e86f3b8f1407b51dad5f88a1291de
Felipe Costa <felipe95176@gmail.com>
2025-04-01 13:56:21 -0300

    Add a not found route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'contact-me', component: ContactMeComponent },
   { path: 'about-me', component: AboutMeComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
