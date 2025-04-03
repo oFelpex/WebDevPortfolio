3a7468626d303f977f17c4553bee0bf2f7cb77b9
Felipe Costa <felipe95176@gmail.com>
2025-04-03 20:15:08 -0300

    ⚡️ Add the new snackBar to contact-me-aside

--- src/app/pages/contact-me/contact-me-aside/contact-me-aside.component.ts
+++ src/app/pages/contact-me/contact-me-aside/contact-me-aside.component.ts
@@ -1,8 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SocialLinksComponent } from '../../../shared/social-links/social-links.component';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AudioService } from '../../../services/audio-service/audio.service';
+import { ThemeService } from '../../../services/theme-service/theme.service';
+import { Themes } from '../../../models/themes';
+import { CustomSnackbarComponent } from '../../../shared/custom-snackbar/custom-snackbar.component';
 
 @Component({
   selector: 'app-contact-me-aside',
@@ -12,46 +16,44 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ContactMeAsideComponent {
   private snackBar: MatSnackBar;
-  private translate: TranslateService;
+  private audioService: AudioService;
+  private themeService: ThemeService;
 
   constructor() {
+    this.themeService = inject(ThemeService);
+    this.audioService = inject(AudioService);
     this.snackBar = inject(MatSnackBar);
-    this.translate = inject(TranslateService);
+  }
+
+  public get getNameOfActualThemeFromLocalStorage(): Themes {
+    return this.themeService.getNameOfActualTheme();
+  }
+  public playClickSound(themeName: string) {
+    this.audioService.playClickSound(themeName);
   }
 
   public clickToCopy(text: string) {
     switch (text) {
       case 'email':
         navigator.clipboard.writeText(`felipe95176@gmail.com`);
-        if (this.translate.currentLang === 'en-US') {
-          this.snackBar.open('Email copied to clipboard!', 'Close', {
-            duration: 4000,
-          });
-        } else {
-          this.snackBar.open(
-            'E-mail copiado para a área de transferência!',
-            'Fechar',
-            {
-              duration: 4000,
-            }
-          );
-        }
+        this.snackBar.openFromComponent(CustomSnackbarComponent, {
+          data: {
+            message: 'SNACK-BAR.CONTACT-PAGE.COPY-EMAIL',
+            theme: this.getNameOfActualThemeFromLocalStorage.name,
+          },
+          duration: 4000,
+        });
+
         break;
       case 'number':
         navigator.clipboard.writeText(`+55 84 98709-5902`);
-        if (this.translate.currentLang === 'en-US') {
-          this.snackBar.open('Number copied to clipboard!', 'Close', {
-            duration: 4000,
-          });
-        } else {
-          this.snackBar.open(
-            'Número copiado para a área de transferência!',
-            'Fechar',
-            {
-              duration: 4000,
-            }
-          );
-        }
+        this.snackBar.openFromComponent(CustomSnackbarComponent, {
+          data: {
+            message: 'SNACK-BAR.CONTACT-PAGE.COPY-NUMBER',
+            theme: this.getNameOfActualThemeFromLocalStorage.name,
+          },
+          duration: 4000,
+        });
         break;
     }
   }
