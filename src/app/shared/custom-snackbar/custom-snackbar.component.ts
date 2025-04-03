4d1cb27b9de757d2a0dad6e747d0ac5e176a546b
Felipe Costa <felipe95176@gmail.com>
2025-04-03 20:10:58 -0300

    ✨ Create a custom snackBar, so I can add methods to the button that close the snackBar, to play sounds for example

---
+++ src/app/shared/custom-snackbar/custom-snackbar.component.ts
@@ -0,0 +1,43 @@
+import { Component, Inject, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme-service/theme.service';
+import { AudioService } from '../../services/audio-service/audio.service';
+import { Themes } from '../../models/themes';
+import {
+  MAT_SNACK_BAR_DATA,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-custom-snackbar',
+  imports: [MatButtonModule, TranslateModule],
+  templateUrl: './custom-snackbar.component.html',
+  styleUrl: './custom-snackbar.component.scss',
+})
+export class CustomSnackbarComponent {
+  private audioService: AudioService;
+  private themeService: ThemeService;
+  private snackBarRef: MatSnackBarRef<CustomSnackbarComponent>;
+  message: string;
+  themeNameMessage: string;
+
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) {
+    this.themeService = inject(ThemeService);
+    this.audioService = inject(AudioService);
+    this.snackBarRef = inject(MatSnackBarRef<CustomSnackbarComponent>);
+    this.message = data.message;
+    this.themeNameMessage = data.themeNameMessage;
+  }
+
+  public get getNameOfActualThemeFromLocalStorage(): Themes {
+    return this.themeService.getNameOfActualTheme();
+  }
+  public playClickSound(themeName: string) {
+    this.audioService.playClickSound(themeName);
+  }
+
+  public dismiss(): void {
+    this.snackBarRef.dismiss();
+  }
+}
