c6a937ae13e22ef546855cbadea3914056c7e165
Felipe Costa <felipe95176@gmail.com>
2025-04-07 16:38:19 -0300

    ✨ Create soundboard component

---
+++ src/app/shared/soundboard/soundboard/soundboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soundboard',
+  imports: [],
+  templateUrl: './soundboard.component.html',
+  styleUrl: './soundboard.component.scss'
+})
+export class SoundboardComponent {
+
+}
