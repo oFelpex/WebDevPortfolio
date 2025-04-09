bc5e0b97fb047a28b46313f9e9bceaf632744135
Felipe Costa <felipe95176@gmail.com>
2025-04-09 20:46:26 -0300

    🎨 Add a musics and sound effects files with a array of each one themetized

---
+++ src/app/models/musics.ts
@@ -0,0 +1,33 @@
+export interface Musics {
+  musicName: string;
+  musicComposer: string;
+  musicURL: string;
+}
+
+export const minecraftMusics: Musics[] = [
+  {
+    musicName: 'Aria Mat',
+    musicComposer: 'C418',
+    musicURL: 'assets/sounds/games/minecraft/music/c418-aria-math.ogg',
+  },
+  {
+    musicName: 'Mice on Venus',
+    musicComposer: 'C418',
+    musicURL: 'assets/sounds/games/minecraft/music/c418-mice-on-venus.ogg',
+  },
+  {
+    musicName: 'Subwoofer Lullaby',
+    musicComposer: 'C418',
+    musicURL: 'assets/sounds/games/minecraft/music/c418-subwoofer-lullaby.ogg',
+  },
+  {
+    musicName: 'Moog City',
+    musicComposer: 'C418',
+    musicURL: 'assets/sounds/games/minecraft/music/c418-moog-city.ogg',
+  },
+  {
+    musicName: 'Pigstep',
+    musicComposer: 'Lena Raine',
+    musicURL: 'assets/sounds/games/minecraft/music/lena-raine-pigstep.ogg',
+  },
+];
---
+++ src/app/models/sfx.ts
@@ -0,0 +1,19 @@
+export interface SFX {
+  SFXName: string;
+  SFXURL: string;
+}
+
+export const minecraftSFX: SFX[] = [
+  {
+    SFXName: 'Minecraft-clickSound',
+    SFXURL: 'assets/sounds/games/minecraft/sfx/button-click.ogg',
+  },
+  {
+    SFXName: 'Minecraft-tnt-activate',
+    SFXURL: 'assets/sounds/games/minecraft/easter-egg/tnt-activate.ogg',
+  },
+  {
+    SFXName: 'Minecraft-tnt-explosion',
+    SFXURL: 'assets/sounds/games/minecraft/easter-egg/tnt-explosion.ogg',
+  },
+];
