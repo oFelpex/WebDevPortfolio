aabe897958813cb2ad5812844d0144fc2ef8a08b
Felipe Costa <felipe95176@gmail.com>
2025-04-11 21:38:06 -0300

    🐛 Fix the bug that flick when soundboard is generated;
    🎨 Add the arrow using mat-icon;
    
    💫 Add a translate animation when click;
    
    ✨ Add click sounds to button.

--- src/app/shared/soundboard/show-soundboard-button/show-soundboard-button.component.ts
+++ src/app/shared/soundboard/show-soundboard-button/show-soundboard-button.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
-import { SoundboardComponent } from './soundboard/soundboard.component';
+import { MatIconModule } from '@angular/material/icon';
 
+import { SoundboardComponent } from './soundboard/soundboard.component';
+import { getUp_return } from '../../animations/translate-animations';
+import { Themes } from '../../../models/themes';
+import { AudioService } from '../../../services/audio-service/audio.service';
+import { ThemeService } from '../../../services/theme-service/theme.service';
 @Component({
   selector: 'app-show-soundboard-button',
-  imports: [MatButtonModule, SoundboardComponent],
+  imports: [MatButtonModule, MatIconModule, SoundboardComponent],
   templateUrl: './show-soundboard-button.component.html',
   styleUrl: './show-soundboard-button.component.scss',
+  animations: [getUp_return],
 })
 export class ShowSoundboardButtonComponent {
+  private audioService: AudioService;
+  private themeService: ThemeService;
+
   public showSoundboard: boolean = false;
+
+  constructor() {
+    this.audioService = inject(AudioService);
+    this.themeService = inject(ThemeService);
+  }
+
+  public get getNameOfActualThemeFromLocalStorage(): Themes {
+    return this.themeService.getNameOfActualTheme();
+  }
+  public playClickSound(themeName: string) {
+    this.audioService.playClickSound(themeName);
+  }
 }
