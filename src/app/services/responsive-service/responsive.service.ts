c6904fa7c5860687628554f9eb7bd077ae78014e
Felipe Costa <felipe95176@gmail.com>
2025-04-12 15:29:21 -0300

    ✨ Create a service to share a boolean, isMobile.

---
+++ src/app/services/responsive-service/responsive.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, debounceTime, fromEvent, map, startWith } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ResponsiveService {
+  private isMobileSubject = new BehaviorSubject<boolean>(
+    window.innerWidth < 820
+  );
+  isMobile$ = this.isMobileSubject.asObservable();
+
+  constructor() {
+    fromEvent(window, 'resize')
+      .pipe(
+        debounceTime(200),
+        map((event: any) => event.target.innerWidth < 820),
+        startWith(window.innerWidth < 820)
+      )
+      .subscribe(this.isMobileSubject);
+  }
+}
