d0d65cac9da70c8439dbca3fcd010395531f7d93
Felipe Costa <felipe95176@gmail.com>
2025-04-13 16:02:15 -0300

    🚧 Prepare soundboard component.

--- src/app/shared/components/header/mobile-soundboard/mobile-soundboard.component.ts
+++ src/app/shared/components/header/mobile-soundboard/mobile-soundboard.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, inject, ViewChild } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AudioService } from '../../../../services/audio-service/audio.service';
+import { ThemeService } from '../../../../services/theme-service/theme.service';
+import { MobileSoundboardMenuService } from '../../../../services/mobile-soundboard-menu/mobile-soundboard-menu.service';
+import { Themes } from '../../../../models/themes';
 
 @Component({
   selector: 'app-mobile-soundboard',
-  imports: [],
+  imports: [MatSidenavModule, MatButtonModule, TranslateModule],
   templateUrl: './mobile-soundboard.component.html',
-  styleUrl: './mobile-soundboard.component.scss'
+  styleUrl: './mobile-soundboard.component.scss',
 })
-export class MobileSoundboardComponent {
+export class MobileSoundboardComponent implements AfterViewChecked {
+  @ViewChild('mobileSoundboard') mobileSoundboard!: MatDrawer;
 
+  private mobileSoundboardMenuService: MobileSoundboardMenuService;
+  private audioService: AudioService;
+  private themeService: ThemeService;
+
+  constructor() {
+    this.mobileSoundboardMenuService = inject(MobileSoundboardMenuService);
+    this.audioService = inject(AudioService);
+    this.themeService = inject(ThemeService);
+  }
+
+  ngAfterViewChecked() {
+    this.mobileSoundboardMenuService.setMobileSoundboardDrawer(
+      this.mobileSoundboard
+    );
+
+    this.mobileSoundboardMenuService.changeMobileSoundboardPointerEvents();
+  }
+
+  public get getNameOfActualThemeFromLocalStorage(): Themes {
+    return this.themeService.getNameOfActualTheme();
+  }
+  public playClickSound(themeName: string) {
+    this.audioService.playClickSound(themeName);
+  }
 }
