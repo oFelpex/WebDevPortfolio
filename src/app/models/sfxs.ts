492a40b2e7799c96924d59d0aa1198b26a1c9ed7
Felipe Costa <felipe95176@gmail.com>
2025-04-19 19:15:55 -0300

    🍱 Add button-click SFX to the witcher 3 theme

--- src/app/models/sfxs.ts
+++ src/app/models/sfxs.ts
@@ -20,7 +20,11 @@ export const minecraftSFX: SFXs[] = [
 
 export const TW3SFX: SFXs[] = [
   {
-    SFXName: 'TW3-igni',
+    SFXName: 'The Witcher 3-igni',
     SFXURL: 'assets/sounds/games/the-witcher-3/easter-egg/igni-sfx.ogg',
+  },
+  {
+    SFXName: 'The Witcher 3-clickSound',
+    SFXURL: 'assets/sounds/games/the-witcher-3/sfx/button-click.ogg',
   },
 ];
