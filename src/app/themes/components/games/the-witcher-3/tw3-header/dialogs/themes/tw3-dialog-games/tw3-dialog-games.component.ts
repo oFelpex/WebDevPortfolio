852f934bdbb211b23b5ff47526f88f7e635f2235
Felipe Costa <felipe95176@gmail.com>
2025-04-19 15:00:59 -0300

    ✨ Now we can also change to another video game theme from the witcher 3 theme

--- src/app/themes/components/games/the-witcher-3/tw3-header/dialogs/themes/tw3-dialog-games/tw3-dialog-games.component.ts
+++ src/app/themes/components/games/the-witcher-3/tw3-header/dialogs/themes/tw3-dialog-games/tw3-dialog-games.component.ts
@@ -3,12 +3,14 @@ import { Component, inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogClose } from '@angular/material/dialog';
 
 import { ThemeService } from '../../../../../../../../services/theme-service/theme.service';
+import { Themes } from '../../../../../../../../models/themes';
 
 @Component({
   selector: 'app-tw3-dialog-games',
-  imports: [MatListModule, MatDividerModule, MatButtonModule],
+  imports: [MatListModule, MatDividerModule, MatButtonModule, MatDialogClose],
   templateUrl: './tw3-dialog-games.component.html',
   styleUrls: ['./tw3-dialog-games.component.scss', '../../dialog.scss'],
 })
@@ -24,5 +26,9 @@ export class Tw3DialogGamesComponent {
   }
   public get actualTheme() {
     return this.themeService.getNameOfActualTheme().name;
+  }
+
+  public changeTheme(theme: Themes) {
+    this.themeService.changeTheme(theme);
   }
 }
