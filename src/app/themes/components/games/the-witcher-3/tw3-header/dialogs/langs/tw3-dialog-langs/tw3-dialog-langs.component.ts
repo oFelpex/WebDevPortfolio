21ab5dd049c30f9d3c821cbee0a984a8db1bd8b1
Felipe Costa <felipe95176@gmail.com>
2025-04-19 14:50:59 -0300

    ✨ Crate the whole system to change language

--- src/app/themes/components/games/the-witcher-3/tw3-header/dialogs/langs/tw3-dialog-langs/tw3-dialog-langs.component.ts
+++ src/app/themes/components/games/the-witcher-3/tw3-header/dialogs/langs/tw3-dialog-langs/tw3-dialog-langs.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tw3-dialog-langs',
-  imports: [],
+  imports: [MatListModule, MatButtonModule],
   templateUrl: './tw3-dialog-langs.component.html',
   styleUrls: ['./tw3-dialog-langs.component.scss', '../../dialog.scss'],
 })
-export class Tw3DialogLangsComponent {}
+export class Tw3DialogLangsComponent {
+  private translateService: TranslateService;
+  private translateSubscribe!: Subscription;
+  public allLangs: string[] = [];
+  public currentLang!: string;
+
+  constructor() {
+    this.translateService = inject(TranslateService);
+    this.allLangs = this.translateService.getLangs();
+  }
+
+  ngOnInit(): void {
+    this.currentLang = this.translateService.currentLang;
+    this.translateSubscribe = this.translateService.onLangChange.subscribe(
+      () => {
+        this.currentLang = this.translateService.currentLang;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.translateSubscribe.unsubscribe();
+  }
+
+  public changeLang(lang: string) {
+    this.translateService.use(lang);
+  }
+}
