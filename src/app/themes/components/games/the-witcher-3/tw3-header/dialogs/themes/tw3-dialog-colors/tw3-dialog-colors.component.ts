840f66610c4d4784824816a641f2984de9cb014d
Felipe Costa <felipe95176@gmail.com>
2025-04-19 15:00:18 -0300

    ✨ Now we can change colors theme from the witcher 3 theme

--- src/app/themes/components/games/the-witcher-3/tw3-header/dialogs/themes/tw3-dialog-colors/tw3-dialog-colors.component.ts
+++ src/app/themes/components/games/the-witcher-3/tw3-header/dialogs/themes/tw3-dialog-colors/tw3-dialog-colors.component.ts
@@ -3,12 +3,14 @@ import { Component, inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogClose } from '@angular/material/dialog';
 
 import { ThemeService } from '../../../../../../../../services/theme-service/theme.service';
+import { Themes } from '../../../../../../../../models/themes';
 
 @Component({
   selector: 'app-tw3-dialog-colors',
-  imports: [MatDividerModule, MatButtonModule, MatListModule],
+  imports: [MatDividerModule, MatButtonModule, MatListModule, MatDialogClose],
   templateUrl: './tw3-dialog-colors.component.html',
   styleUrls: ['./tw3-dialog-colors.component.scss', '../../dialog.scss'],
 })
@@ -19,10 +21,14 @@ export class Tw3DialogColorsComponent {
     this.themeService = inject(ThemeService);
   }
 
-  public get colorsOptions() {
+  public get colorsOptions(): Themes[] {
     return this.themeService.getColorsNames();
   }
-  public get actualTheme() {
+  public get actualTheme(): string {
     return this.themeService.getNameOfActualTheme().name;
+  }
+
+  public changeTheme(theme: Themes) {
+    this.themeService.changeTheme(theme);
   }
 }
