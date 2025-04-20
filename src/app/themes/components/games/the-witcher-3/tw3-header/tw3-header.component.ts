94315f10416daec5d022c78b128008a8ba83c6d2
Felipe Costa <felipe95176@gmail.com>
2025-04-19 22:44:01 -0300

    ♿️ ✨ Add Mobile styles, navBar, buttons, menus...

--- src/app/themes/components/games/the-witcher-3/tw3-header/tw3-header.component.ts
+++ src/app/themes/components/games/the-witcher-3/tw3-header/tw3-header.component.ts
@@ -4,14 +4,26 @@ import { Subscription } from 'rxjs';
 import { NavigationEnd, Router } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 
 import { LogoEffectsComponent } from '../../../../../logo-effects/logo-effects.component';
 import { DialogsComponent } from './dialogs/dialogs.component';
 import { AudioService } from '../../../../../services/audio-service/audio.service';
+import { MobileSoundboardMenuService } from '../../../../../services/mobile-soundboard-menu/mobile-soundboard-menu.service';
+import { MobileNavMenuService } from '../../../../../services/mobile-menu-service/mobile-nav-menu.service';
+import { ResponsiveService } from '../../../../../services/responsive-service/responsive.service';
+import { MobileSoundboardComponent } from '../../../../../shared/components/header/mobile-soundboard/mobile-soundboard.component';
+import { MobileMenuComponent } from '../../../../../shared/components/header/mobile-menu/mobile-menu.component';
 @Component({
   selector: 'app-tw3-header',
-  imports: [MatButtonModule, LogoEffectsComponent],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    LogoEffectsComponent,
+    MobileMenuComponent,
+    MobileSoundboardComponent,
+  ],
   templateUrl: './tw3-header.component.html',
   styleUrl: './tw3-header.component.scss',
 })
@@ -20,6 +32,11 @@ export class Tw3NavBarComponent implements OnInit, OnDestroy {
   private routerSubscription!: Subscription;
   private dialog: MatDialog;
   private audioService: AudioService;
+  private mobileNavMenuService: MobileNavMenuService;
+  private mobileSoundboardMenuService: MobileSoundboardMenuService;
+  private responsiveService: ResponsiveService;
+
+  public isMobile: boolean = window.innerWidth <= 820;
   public currentRoute!: string;
   public title!: string;
   public allRoutes: string[] = ['ABOUT-ME', 'HOME', 'PROJECTS', 'CONTACT-ME'];
@@ -29,6 +46,9 @@ export class Tw3NavBarComponent implements OnInit, OnDestroy {
     this.router = inject(Router);
     this.dialog = inject(MatDialog);
     this.audioService = inject(AudioService);
+    this.mobileNavMenuService = inject(MobileNavMenuService);
+    this.mobileSoundboardMenuService = inject(MobileSoundboardMenuService);
+    this.responsiveService = inject(ResponsiveService);
   }
 
   ngOnInit() {
@@ -46,6 +66,19 @@ export class Tw3NavBarComponent implements OnInit, OnDestroy {
         this.title = this.currentRoute.slice(1).toUpperCase();
         if (!this.allRoutes.some(routeExist)) this.title = 'SECRET PLACE?';
         this.setCurrentIndexByRoute();
+      }
+    });
+
+    this.responsiveService.isMobile$.subscribe((isMobile) => {
+      this.isMobile = isMobile;
+      if (!this.isMobile) {
+        if (this.mobileNavMenuService.mobileNavMenu)
+          if (this.mobileNavMenuService.mobileNavMenu.opened)
+            this.mobileNavMenuService.toggleMobileNavMenu();
+
+        if (this.mobileSoundboardMenuService.mobileSoundboardMenu)
+          if (this.mobileSoundboardMenuService.mobileSoundboardMenu.opened)
+            this.mobileSoundboardMenuService.toggleMobileSoundboardMenu();
       }
     });
   }
@@ -92,5 +125,30 @@ export class Tw3NavBarComponent implements OnInit, OnDestroy {
         dialogType: 'Langs',
       },
     });
+  }
+
+  public toggleMobileNavMenu() {
+    if (this.mobileSoundboardMenuService.mobileSoundboardMenu) {
+      if (this.mobileSoundboardMenuService.mobileSoundboardMenu.opened) {
+        this.mobileNavMenuService.toggleMobileNavMenu();
+        this.mobileSoundboardMenuService.toggleMobileSoundboardMenu();
+        return;
+      }
+      this.mobileNavMenuService.toggleMobileNavMenu();
+      return;
+    }
+    this.mobileNavMenuService.toggleMobileNavMenu();
+  }
+  public toggleMobileSoundboardMenu() {
+    if (this.mobileNavMenuService.mobileNavMenu) {
+      if (this.mobileNavMenuService.mobileNavMenu.opened) {
+        this.mobileSoundboardMenuService.toggleMobileSoundboardMenu();
+        this.mobileNavMenuService.toggleMobileNavMenu();
+        return;
+      }
+      this.mobileSoundboardMenuService.toggleMobileSoundboardMenu();
+      return;
+    }
+    this.mobileSoundboardMenuService.toggleMobileSoundboardMenu();
   }
 }
