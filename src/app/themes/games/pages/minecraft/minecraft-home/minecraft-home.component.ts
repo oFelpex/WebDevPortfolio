b169b90b1ca13b8368adeb07ebeef34315d904c3
Felipe Costa <felipe95176@gmail.com>
2025-05-01 12:01:01 -0300

    ✨ Add a whole structure to change some styles property of soundboard

--- src/app/themes/games/pages/minecraft/minecraft-home/minecraft-home.component.ts
+++ src/app/themes/games/pages/minecraft/minecraft-home/minecraft-home.component.ts
@@ -12,10 +12,18 @@ import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { AudioService } from '../../../../../services/audio-service/audio.service';
 import { ThemeService } from '../../../../../services/theme-service/theme.service';
+import { SoundboardComponent } from '../../../../../shared/components/soundboard/show-soundboard-button/soundboard/soundboard.component';
+import { Subscription } from 'rxjs';
+import { ResponsiveService } from '../../../../../services/responsive-service/responsive.service';
 
 @Component({
   selector: 'app-minecraft-home',
-  imports: [LogoEffectsComponent, RouterModule, TranslateModule],
+  imports: [
+    LogoEffectsComponent,
+    RouterModule,
+    TranslateModule,
+    SoundboardComponent,
+  ],
   templateUrl: './minecraft-home.component.html',
   styleUrl: './minecraft-home.component.scss',
 })
@@ -23,13 +31,18 @@ export class MinecraftHomeComponent implements OnInit, OnDestroy {
   @ViewChild('canvasContainer', { static: true }) canvasContainer!: ElementRef;
 
   public phrase!: string;
+  public showSoundboard: boolean = false;
+  public isMobile: boolean = false;
 
+  private showSoundboardSubscription!: Subscription;
+  private responsiveSubscription!: Subscription;
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
   private skybox!: THREE.Mesh;
   private audioService: AudioService;
   private themeService: ThemeService;
+  private responsiveService: ResponsiveService;
   private listOfPhrases: string[] = [
     'ONE',
     'TWO',
@@ -44,6 +57,12 @@ export class MinecraftHomeComponent implements OnInit, OnDestroy {
     'ELEVEN',
   ];
 
+  constructor() {
+    this.themeService = inject(ThemeService);
+    this.audioService = inject(AudioService);
+    this.responsiveService = inject(ResponsiveService);
+  }
+
   ngOnInit(): void {
     const header = document.getElementById(
       'minecraft-header'
@@ -52,6 +71,30 @@ export class MinecraftHomeComponent implements OnInit, OnDestroy {
     header.style.visibility = 'hidden';
     header.style.height = '0px';
     header.style.minHeight = '0px';
+
+    this.soundboardSetPosition();
+    this.responsiveSubscription = this.responsiveService.isMobile$.subscribe(
+      (isMobile) => {
+        this.isMobile = isMobile;
+
+        if (!this.isMobile) {
+          this.audioService.setShowSoundboard(false);
+
+          setTimeout(() => {
+            this.soundboardSetPosition();
+          });
+        }
+
+        if (isMobile) {
+          this.audioService.setShowSoundboard(false);
+        }
+      }
+    );
+
+    this.showSoundboardSubscription =
+      this.audioService.showSoundboard$.subscribe((value) => {
+        this.showSoundboard = value;
+      });
 
     this.phrase = this.listOfPhrases[this.phraseNumber()];
 
@@ -64,15 +107,38 @@ export class MinecraftHomeComponent implements OnInit, OnDestroy {
       'minecraft-header'
     ) as HTMLHeadingElement;
 
+    this.soundboardRestaurePosition();
+
     header.style.display = 'block';
     header.style.visibility = 'visible';
     header.style.height = '14vh';
     header.style.minHeight = '60px';
+
+    this.showSoundboardSubscription.unsubscribe();
+    this.responsiveSubscription.unsubscribe();
   }
 
-  constructor() {
-    this.themeService = inject(ThemeService);
-    this.audioService = inject(AudioService);
+  private soundboardSetPosition() {
+    const soundboard = document.getElementById('soundboard') as HTMLDivElement;
+    if (soundboard) {
+      soundboard.style.right = '50%';
+      soundboard.style.transform = 'translate(50%, -10%)';
+    }
+  }
+  private soundboardRestaurePosition() {
+    const soundboard = document.getElementById('soundboard') as HTMLDivElement;
+    if (soundboard) {
+      soundboard.style.right = '20px';
+      soundboard.style.transform = '';
+    }
+  }
+
+  public toggleSoundboard(): void {
+    if (this.isMobile) {
+      // ADD MOBILE MENU TOGGLE HERE
+      return;
+    }
+    this.audioService.toggleSoundboard();
   }
 
   public quitGame(): void {
