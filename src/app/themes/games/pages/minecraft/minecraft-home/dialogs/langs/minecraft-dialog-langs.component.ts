0ee5d37edf1adc723bb683281b52d23e9abe4d0d
Felipe Costa <felipe95176@gmail.com>
2025-05-01 21:44:40 -0300

    ✨ Add the dialog content for langs

--- src/app/themes/games/pages/minecraft/minecraft-home/dialogs/langs/minecraft-dialog-langs.component.ts
+++ src/app/themes/games/pages/minecraft/minecraft-home/dialogs/langs/minecraft-dialog-langs.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import { AudioService } from '../../../../../../../services/audio-service/audio.service';
 
 @Component({
   selector: 'app-minecraft-dialog-langs',
-  imports: [],
+  imports: [MatListModule, MatButtonModule, FormsModule, MatRadioModule],
   templateUrl: './minecraft-dialog-langs.component.html',
   styleUrl: './minecraft-dialog-langs.component.scss',
 })
-export class MinecraftDialogLangsComponent {}
+export class MinecraftDialogLangsComponent {
+  private translateService: TranslateService;
+  private audioService: AudioService;
+  private translateSubscribe!: Subscription;
+  public allLangs: string[] = [];
+  public currentLang!: string;
+
+  constructor() {
+    this.translateService = inject(TranslateService);
+    this.audioService = inject(AudioService);
+    this.allLangs = this.translateService.getLangs();
+  }
+
+  ngOnInit(): void {
+    this.currentLang = this.translateService.currentLang;
+    this.translateSubscribe = this.translateService.onLangChange.subscribe(
+      () => {
+        this.currentLang = this.translateService.currentLang;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.translateSubscribe.unsubscribe();
+  }
+
+  public playClickSound(): void {
+    this.audioService.playClickSound('Minecraft');
+  }
+
+  public changeLang(lang: string) {
+    this.translateService.use(lang);
+  }
+}
