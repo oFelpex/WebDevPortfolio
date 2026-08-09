0a618a389a0d82020932dbfeaa68f3abc519b58a
Felipe Costa <felipe95176@gmail.com>
2026-08-09 17:16:33 -0300

    💄 Refactor themes.ts temporary for improved responsive design; adjust spacing and remove commented-out code

--- src/app/models/themes.ts
+++ src/app/models/themes.ts
@@ -1,12 +1,12 @@
 export type Games =
   | 'Red Dead Redemption 2'
-  | 'The Witcher 3' // Work in progress 
+  | 'The Witcher 3' // Work in progress
   | 'The Legend of Zelda: BOTW' // Not sure, but might be Zelda in general
-  | 'Persona 3'// Could be Persona 5, still thinking about it
+  | 'Persona 3' // Could be Persona 5, still thinking about it
   | 'Elden Ring' // It might be Dark Souls instead
   | 'Hollow Knight'
   | 'Undertale'
-  | 'Minecraft'; // Done! 
+  | 'Minecraft'; // Done!
 export type Colors = 'Light' | 'Dark' | 'Cosmic';
 
 export type ThemeCategory = Games | Colors;
@@ -15,34 +15,34 @@ export interface Themes {
   type: 'Games' | 'Colors';
 }
 export const gamesOptions: Themes[] = [
-  // {
-  //   name: 'Red Dead Redemption 2',
-  //   type: 'Games',
-  // },
+  {
+    name: 'Red Dead Redemption 2',
+    type: 'Games',
+  },
   {
     name: 'The Witcher 3',
     type: 'Games',
   },
-  // {
-  //   name: 'Cyberpunk',
-  //   type: 'Games'
-  // },
-  // {
-  //   name: 'Elden Ring',
-  //   type: 'Games'
-  // },
-  // {
-  //   name: 'Hollow Knight',
-  //   type: 'Games'
-  // },
-  // {
-  //   name: 'Undertale',
-  //   type: 'Games'
-  // },
-  // {
-  //   name: 'The Legend of Zelda: BOTW',
-  //   type: 'Games',
-  // }
+  {
+    name: 'The Legend of Zelda: BOTW',
+    type: 'Games',
+  },
+  {
+    name: 'Persona 3',
+    type: 'Games',
+  },
+  {
+    name: 'Elden Ring',
+    type: 'Games',
+  },
+  {
+    name: 'Hollow Knight',
+    type: 'Games',
+  },
+  {
+    name: 'Undertale',
+    type: 'Games',
+  },
   {
     name: 'Minecraft',
     type: 'Games',
