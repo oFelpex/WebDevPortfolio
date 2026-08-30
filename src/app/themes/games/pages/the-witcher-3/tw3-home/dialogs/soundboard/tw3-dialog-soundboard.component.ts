4bfdcc4637417c4714e74f3b3d96acf884d39c35
Felipe Costa <felipe95176@gmail.com>
2026-08-30 20:19:03 -0300

    feat(soundboard): implement song selection and enhance styling for The Witcher 3 theme

--- src/app/themes/games/pages/the-witcher-3/tw3-home/dialogs/soundboard/tw3-dialog-soundboard.component.ts
+++ src/app/themes/games/pages/the-witcher-3/tw3-home/dialogs/soundboard/tw3-dialog-soundboard.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Musics, TW3Musics } from '../../../../../../../models/musics';
 
 @Component({
   selector: 'app-tw3-dialog-soundboard',
   imports: [],
   templateUrl: './tw3-dialog-soundboard.component.html',
-  styleUrl: './tw3-dialog-soundboard.component.scss'
+  styleUrl: './tw3-dialog-soundboard.component.scss',
 })
 export class Tw3DialogSoundboardComponent {
+  public tw3Musics: Musics[] = TW3Musics;
 
+  constructor() {}
+
+  public getCardFileName(tw3MusicName: string): string {
+    const cleanMusicName = tw3MusicName
+      .toLowerCase()
+      .replace(/[^a-z0-9\s]/g, '')
+      .trim()
+      .replace(/\s+/g, '-');
+
+    return `../../../../../../../../assets/themes/games/the witcher 3/cards/${cleanMusicName}-card.webp`;
+  }
 }
